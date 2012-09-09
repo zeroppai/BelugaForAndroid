e024013754b553a3bf20f2a84a7c49f5f7a2fcd5
zero <zero@beluga.fm>
2012-09-09 20:30:46 +0900

    認証部分の機能追加を行った

---
+++ BelugaSample/src/fm/beluga/zero/belugasample/activity/MainActivity.java
@@ -0,0 +1,149 @@
+package fm.beluga.zero.belugasample.activity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import fm.beluga.zero.belugasample.Beluga;
+import fm.beluga.zero.belugasample.R;
+
+public class MainActivity extends Activity {
+	private ListAdapter listAdapter;
+
+	private Beluga beluga = Beluga.Instance();
+	private List<Beluga.Timeline> timeline_list = new ArrayList<Beluga.Timeline>();
+	private Timer timer;
+	private Handler handler = new Handler();
+
+	@Override public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// Check Token
+		if(!beluga.isConnected()){
+			Toast.makeText(this, "トークンが設定されていません", Toast.LENGTH_LONG).show();
+			goOptionAction();
+		}
+
+		List<Beluga.Timeline> list = beluga.getHome();
+		if(list != null) timeline_list.addAll(list);
+
+		// Timer
+		timer = new Timer(true);
+		timer.schedule(new TimerTask() {
+			@Override public void run() {
+				// TODO Auto-generated method stub
+				updateTimeline();
+
+				// 別スレッドでUIにアクセスしたい場合はHandlerクラスを利用する
+				handler.post(new Runnable() {
+					@Override public void run() {
+						// TODO Auto-generated method stub
+						listAdapter.notifyDataSetChanged();
+					}
+				});
+			}
+		}, 10000, 30000);
+
+		listAdapter = new ListAdapter(getApplicationContext(), timeline_list);
+
+		ListView listView = (ListView) findViewById(R.id.listView1);
+		listView.setAdapter(listAdapter);
+	}
+
+	private void updateTimeline() {
+		List<Beluga.Timeline> list = beluga.getHomeFromLast();
+		if (list != null) {
+			Collections.reverse(list);
+			for (Beluga.Timeline tl : list) {
+				timeline_list.add(0, tl);
+			}
+		}
+	}
+	
+	private void goOptionAction() {
+		Intent intent = new Intent(MainActivity.this, OptionActivity.class);
+		intent.setAction(Intent.ACTION_VIEW);
+		startActivity(intent);
+	}
+
+	@Override public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.item1: // 書き込み
+			break;
+		case R.id.item2: // 更新
+			updateTimeline();
+			listAdapter.notifyDataSetChanged();
+			break;
+		case R.id.item3: // ルーム一覧
+			break;
+		case R.id.item4: // 設定
+			goOptionAction();
+			break;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public class ListAdapter extends ArrayAdapter<Beluga.Timeline> {
+		private LayoutInflater mInflater;
+		private TextView mNmae, mText, mOption;
+		private ImageView mIcon;
+
+		public ListAdapter(Context context, List<Beluga.Timeline> objects) {
+			super(context, 0, objects);
+			mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.row, null);
+			}
+
+			final Beluga.Timeline item = this.getItem(position);
+			if (item != null) {
+				mIcon = (ImageView) convertView.findViewById(R.id.icon_image);
+				try {
+					mIcon.setImageBitmap(item.icon_x75);
+				} catch (Exception e) {
+					Log.e("homo", "Icon load error");
+				}
+
+				mNmae = (TextView) convertView.findViewById(R.id.name_text);
+				mNmae.setText(item.name);
+
+				mText = (TextView) convertView.findViewById(R.id.main_text);
+				mText.setText(item.text);
+
+				mOption = (TextView) convertView.findViewById(R.id.option_text);
+				mOption.setText(item.date_string + " - " + item.room_name);
+			}
+			return convertView;
+		}
+	}
+}
---
+++ BelugaSample/src/fm/beluga/zero/belugasample/activity/OptionActivity.java
@@ -0,0 +1,68 @@
+package fm.beluga.zero.belugasample.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import fm.beluga.zero.belugasample.Beluga;
+import fm.beluga.zero.belugasample.R;
+
+public class OptionActivity extends Activity {
+	Beluga beluga = Beluga.Instance();
+	
+	@Override public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_option);
+
+		Button auth_button = (Button) findViewById(R.id.auth_button);
+		auth_button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				doIntent(Intent.ACTION_VIEW, Beluga.auth_url);
+			}
+		});
+
+		Uri uri = getIntent().getData();
+		if (uri != null) {
+			beluga.setUserToken(uri.getQueryParameter("user_id"), uri.getQueryParameter("user_token"));
+			if(beluga.isConnected()){
+				Toast.makeText(this, "認証しました", Toast.LENGTH_LONG).show();
+			}else{
+				Toast.makeText(this, "認証に失敗しました。", Toast.LENGTH_LONG).show();
+			}
+		}
+
+		TextView text = (TextView)findViewById(R.id.auth_label);
+		if (beluga.isConnected()) {
+			text.setText("認証済み");
+		}else{
+			text.setText("未認証");
+		}
+		
+		Button back_button = (Button)findViewById(R.id.option_back_button);
+		back_button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				goMainAction();
+			}
+		});
+	}
+	
+	private void goMainAction() {
+		Intent intent = new Intent(OptionActivity.this, MainActivity.class);
+		intent.setAction(Intent.ACTION_VIEW);
+		startActivity(intent);
+	}
+	
+	private void doIntent(String action, String uriString) {
+		try {
+			Intent intent = new Intent(action, Uri.parse(uriString));
+			startActivity(intent);
+		} catch (Exception e) {
+			Toast.makeText(this, "失敗\n" + e.getMessage(), Toast.LENGTH_LONG).show();
+		}
+	}
+
+}
