c91e7daa65ca9f07ee53ad22abc96b3bd99f4b0a
zero <zero@beluga.fm>
2012-09-12 01:04:07 +0900

    ギャラリーからアップロードできるようにした

--- BelugaSample/src/fm/beluga/zero/belugasample/activity/UpdateActivity.java
+++ BelugaSample/src/fm/beluga/zero/belugasample/activity/UpdateActivity.java
@@ -1,8 +1,10 @@
 package fm.beluga.zero.belugasample.activity;
 
+import java.io.InputStream;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -19,40 +21,41 @@ import fm.beluga.zero.belugasample.Beluga;
 import fm.beluga.zero.belugasample.R;
 
 public class UpdateActivity extends Activity {
+	final int REQUEST_GALLERY = 0; 
 	Beluga beluga = Beluga.Instance();
 	EditText editor;
 	Spinner sp;
-	
+
 	@Override public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_update);
-		
+
 		// Spinner
 		List<Beluga.Room> list = beluga.getRoomList();
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.spinner_item);
-		
+
 		adapter.setDropDownViewResource(R.layout.new_simple_spinner_dropdown_item);
-		for(int i=0;i<list.size();i++){
+		for (int i = 0; i < list.size(); i++) {
 			Beluga.Room room = list.get(i);
 			adapter.add(room.name);
 		}
-		
-		sp = (Spinner)findViewById(R.id.spinner1);
+
+		sp = (Spinner) findViewById(R.id.spinner1);
 		sp.setAdapter(adapter);
 		sp.setSelection(0);
-		
+
 		// Editor
-		editor = (EditText)findViewById(R.id.edit_text_view);
-		
+		editor = (EditText) findViewById(R.id.edit_text_view);
+
 		// Send Button
-		Button button = (Button)findViewById(R.id.edit_update_button);
+		Button button = (Button) findViewById(R.id.edit_update_button);
 		button.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				EditText edit = (EditText)findViewById(R.id.edit_text_view);
-				SpannableStringBuilder sb = (SpannableStringBuilder)edit.getText();
-				if(sb.toString()!=""){
+				EditText edit = (EditText) findViewById(R.id.edit_text_view);
+				SpannableStringBuilder sb = (SpannableStringBuilder) edit.getText();
+				if (sb.toString() != "") {
 					Beluga.Room room = beluga.getRoomList().get(sp.getSelectedItemPosition());
-					beluga.postText(room.hash,sb.toString());
+					beluga.postText(room.hash, sb.toString());
 					edit.setText("");
 					Toast.makeText(getApplicationContext(), "投稿しました", Toast.LENGTH_LONG).show();
 				}
@@ -60,13 +63,32 @@ public class UpdateActivity extends Activity {
 		});
 
 		// Upload Image
-		Button upload_image_button = (Button)findViewById(R.id.upload_image_button);
+		Button upload_image_button = (Button) findViewById(R.id.upload_image_button);
 		upload_image_button.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
-				ImgUrl data = ImgurAPI.uploadImage(bmp);
-				editor.setText(editor.getText() + " " + data.url);
+				// ギャラリー呼び出し
+				Intent intent = new Intent();
+				intent.setType("image/*");
+				intent.setAction(Intent.ACTION_GET_CONTENT);
+				startActivityForResult(intent, REQUEST_GALLERY);
 			}
 		});
+	}
+
+	@Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		if (requestCode == REQUEST_GALLERY && resultCode == RESULT_OK) {
+			try {
+				InputStream in = getContentResolver().openInputStream(data.getData());
+				Bitmap img = BitmapFactory.decodeStream(in);
+				in.close();
+
+				// 選択した画像のりよう
+				ImgUrl imgurl = ImgurAPI.uploadImage(img);
+				editor.setText(editor.getText() + " " + imgurl.url);
+			} catch (Exception e) {
+
+			}
+		}
 	}
 }
