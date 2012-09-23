484e8b2df605918ea6e7b4c5e099dabc6afc2737
zero <zero@beluga.fm>
2012-09-23 22:10:08 +0900

    v4で認証できない不具合を修正した

--- BelugaSample/src/fm/beluga/zero/belugasample/activity/MainActivity.java
+++ BelugaSample/src/fm/beluga/zero/belugasample/activity/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		// for over 3.0
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
 		}
 
